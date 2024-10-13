5ffb76a9f75eb2f3640a005fb733012bfecc4442
Aktan <aktan.abykeev@gmail.com>
2024-10-13 14:06:01 +0300

    skip

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import os
 
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 print("Secret Key:", app.config['SECRET_KEY'])  # Debugging line
@@ -45,7 +44,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
